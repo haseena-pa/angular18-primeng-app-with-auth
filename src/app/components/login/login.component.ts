05d5c876645c708472f1a66759449fd7712d35aa
haseena-pa <haseenakhader.uk@gmail.com>
2024-09-25 16:46:52 +0100

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    InputTextModule,
+    FormsModule,
+    PasswordModule,
+    ButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  login = {
+    email: '',
+    password: '',
+  };
 
+  onLogin() {
+    console.log(this.login);
+  }
 }
